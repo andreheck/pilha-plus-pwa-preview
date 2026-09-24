325e4b9d900b09ef69b7f2274718f8f07dff993b
Andre Heck <107319452+andreheck@users.noreply.github.com>
2026-09-24 16:04:39 -0300

    Fix camera sampling and low FPS recovery

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'pilha-app-shell';
-const BUILD_ID = '20260924-cc75e50';
+const BUILD_ID = '20260924-ed46ce3';
 const CACHE_NAME = `${CACHE_PREFIX}-v${BUILD_ID}`;
 const BASE = '/pilha-plus-pwa-preview';
 const SHELL_HTML = `${BASE}/index.html`;
