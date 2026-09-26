0dd56b7ea34b5477d9d54788e3f625b5be347a5a
Andre Heck <107319452+andreheck@users.noreply.github.com>
2026-09-26 09:14:10 -0300

    Animate welcome button with gradual liquid colors

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'pilha-app-shell';
-const BUILD_ID = '20260926-8248da6';
+const BUILD_ID = '20260926-780d521';
 const CACHE_NAME = `${CACHE_PREFIX}-v${BUILD_ID}`;
 const BASE = '/pilha-plus-pwa-preview';
 const SHELL_HTML = `${BASE}/index.html`;
