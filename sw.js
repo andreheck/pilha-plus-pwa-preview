d93326cddba3d974277fe8e5656d36b19fe974aa
Andre Heck <107319452+andreheck@users.noreply.github.com>
2026-09-26 14:25:11 -0300

    Add Plano(b) test invitations and platform overview

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'pilha-app-shell';
-const BUILD_ID = '20260926-253bfea';
+const BUILD_ID = '20260926-5c6012c';
 const CACHE_NAME = `${CACHE_PREFIX}-v${BUILD_ID}`;
 const BASE = '/pilha-plus-pwa-preview';
 const SHELL_HTML = `${BASE}/index.html`;
